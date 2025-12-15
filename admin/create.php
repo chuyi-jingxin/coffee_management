a84647bfb98c92e4048bc18dc5c43ee65e02e9a4
Trinh Minh Tam <114578738+fiona-astreia@users.noreply.github.com>
2025-12-15 10:29:32 +0700

    Update create.php
    - create interface for create (add a product) page
    
    Co-Authored-By: ThieuHoai <182103236+ThieuHoai@users.noreply.github.com>

--- admin/create.php
+++ admin/create.php
@@ -98,3 +98,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         mysqli_stmt_close($stmt);
     }
 }
+mysqli_close($con);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
