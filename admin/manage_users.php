1ec542183a0ce0f6c7f6dbbf8b8ff1699731ce00
lynguyen2812 <khanhly122004@gmail.com>
2025-12-15 18:19:54 +0700

    Update manage_users.php
    update

--- admin/manage_users.php
+++ admin/manage_users.php
@@ -55,6 +55,10 @@ if (isset($_GET['delete_id'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <style>
+        body {
+            background: #f8f9fa;
+            font-family: 'Poppins', sans-serif;
+        }
         .container {
             margin-top: 30px;
             background: #fff;
