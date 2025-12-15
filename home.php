c75890c2346f1e6070c94caddaa226e99e20ccc7
Trinh Minh Tam <114578738+fiona-astreia@users.noreply.github.com>
2025-12-15 09:56:20 +0700

    Update home.php

--- home.php
+++ home.php
@@ -37,3 +37,12 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
+/* TÍNH GIỎ HÀNG (Customer) */
+$cart_count = 0;
+if (isset($_SESSION['role']) && $_SESSION['role'] != 'admin') {
+    if (isset($_SESSION['cart'])) {
+        foreach ($_SESSION['cart'] as $item) {
+            $cart_count += $item['quantity'];
+        }
+    }
+}
