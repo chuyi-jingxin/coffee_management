0d7f4d44819a349c9990be37fa6af75272d71a30
Trinh Minh Tam <114578738+chuyi-jingxin@users.noreply.github.com>
2026-01-15 18:17:14 +0700

    Update order_detail.php

--- admin/order_detail.php
+++ admin/order_detail.php
@@ -3,7 +3,6 @@ session_start();
 require_once '../config/db.php';
 // Admin biết khách hàng order những món gì
 
-// 1. CHECK QUYỀN
 if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
     header('location:../home.php?msg=no_permission');
     exit();
@@ -11,7 +10,7 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
 
 $order_id = (int) ($_GET['id'] ?? 0);
 
-// 2. LẤY THÔNG TIN ĐƠN HÀNG (Người mua, địa chỉ...)
+// THÔNG TIN ĐƠN HÀNG
 $stmt = mysqli_prepare($con, "SELECT * FROM orders WHERE id = ?");
 mysqli_stmt_bind_param($stmt, "i", $order_id);
 mysqli_stmt_execute($stmt);
@@ -20,7 +19,7 @@ $order = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
 if (!$order)
     die("Order not found");
 
-// 3. LẤY DANH SÁCH MÓN ĂN (Join với bảng products để lấy tên và ảnh)
+// LẤY DANH SÁCH MÓN ĂN (Join với bảng products để lấy tên và ảnh)
 $query_items = "SELECT oi.*, p.name as product_name, p.image 
                 FROM order_items oi 
                 JOIN products p ON oi.product_id = p.id 
